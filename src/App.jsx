f21294a944d6fe55255079290301001b86903e17
ankush-sihag <ankushsihag7@gmail.com>
2026-07-23 10:23:32 +0530

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import Navbar  from "./components/Navbar/Navbar";
 import MenuBar from "./components/MenuBar/menuBar";
+
 import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Login from "./pages/Login/Login";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
           <Navbar />
           <MenuBar />
-          <Home />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/login" element={<login />} />
+          </Routes>
     </>
   );
 }
