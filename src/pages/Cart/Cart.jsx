d2699bf6043d2829ff21f9a8f1010f0be014c754
ankush-sihag <ankushsihag7@gmail.com>
2026-07-24 15:04:45 +0530

    commit

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -3,11 +3,26 @@ import { CartContext } from "../../context/CartContext";
 
 function Cart() {
     const { cart } = useContext(CartContext);
-    console.log("cart page:", cart);
     return (
         <section>
             <h1>Shopping Cart</h1>
             <h2>Total Items : {cart.length}</h2>
+            {
+                cart.map((product) => {
+                    return (
+                        <div key={product.id}>
+                            <img
+                                src={product.image}
+                                alt={product.name}
+                                width="120"
+                            />
+                            <h3>{product.name}</h3>
+                            <p>₹{product.price}</p>
+                            <p>⭐ {product.rating}</p>
+                        </div>
+                    );
+                })
+            }
         </section>
     );
 }
