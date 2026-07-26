86f7d0f008a709a64cca3f38a4e79b76420971d4
ankush-sihag <ankushsihag7@gmail.com>
2026-07-26 19:38:43 +0530

    commit

--- src/pages/Home/components/FeaturedProducts.jsx
+++ src/pages/Home/components/FeaturedProducts.jsx
@@ -1,30 +1,47 @@
-import products from "../../../data/products";
-import "./FeaturedProducts.css";
+import { useEffect, useState } from "react";
 import ProductCard from "../../../components/ProductCard/ProductCard";
+import { fetchProducts } from "../../../services/product.service";
+import "./FeaturedProducts.css";
 
 function FeaturedProducts() {
-    
+
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        loadFeaturedProducts();
+    }, []);
+
+    async function loadFeaturedProducts() {
+        try {
+            const data = await fetchProducts();
+            setProducts(data.slice(0, 4));
+        }
+        catch (error) {
+            console.error(error);
+        }
+
+        finally {
+            setLoading(false);
+        }
+    }
+
+    if (loading) {
+        return <h2>Loading Featured Products...</h2>;
+    }
+
     return (
         <section className="featured-products">
             <h2>Featured Products</h2>
             <div className="product-grid">
-                {products.map((product)=>(
-                   <ProductCard
-
-                        key={product.id}
-
-                        name={product.name}
-
-                        price={product.price}
-
-                        image={product.image}
-
-                        rating={product.rating}
-
-                        delivery={product.delivery}
-
-                    />
-                ))}
+                {
+                    products.map((product) => (
+                        <ProductCard
+                            key={product._id}
+                            product={product}
+                        />
+                    ))
+                }
             </div>
         </section>
     );
